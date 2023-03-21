419cf4e4e64f157aefe9d48d2a3fb0cb3da0677c
Zeph180 <kigozijosephsuubi1@gmail.com>
2023-03-21 12:11:48 +0300

    Implement useParams

--- src/components/pages/DetailsScreen.js
+++ src/components/pages/DetailsScreen.js
@@ -1,3 +1,70 @@
+import { useSelector } from 'react-redux';
+import { Link, useParams } from 'react-router-dom';
+
 export default function DetailsScreen() {
-  return <p>Details screen</p>;
+  const { cryptoCoins } = useSelector((state) => state.cryptoCoins);
+  const { id } = useParams();
+  const cryptoCoin = cryptoCoins.find((item) => item.id === id);
+  return (
+    <section>
+      <Link to="/">To home</Link>
+      <article>
+        <img
+          src={cryptoCoin.icon}
+          alt={`${cryptoCoin.name}logo`}
+        />
+        <p>
+          Name:
+          {' '}
+          {cryptoCoin.name}
+        </p>
+        <p>
+          Price:
+          {' '}
+          {cryptoCoin.price}
+        </p>
+        <article>
+          <h4>Price Changes</h4>
+          <p>
+            High:
+            {' '}
+            {cryptoCoin.priceChange1h}
+          </p>
+          <p>
+            Diff:
+            {' '}
+            {cryptoCoin.priceChange1h}
+          </p>
+          <p>
+            Low:
+            {' '}
+            {cryptoCoin.priceChange1h}
+          </p>
+        </article>
+        <article>
+          <h4>Market Info</h4>
+          <p>
+            Total Supply:
+            {' '}
+            {cryptoCoin.totalSupply}
+          </p>
+          <p>
+            Availble Supply:
+            {' '}
+            {cryptoCoin.availableSupply}
+          </p>
+          <p>
+            Market Cap:
+            {' '}
+            {cryptoCoin.marketCap}
+          </p>
+          <p>
+            Volume:
+            {' '}
+            {cryptoCoin.volume}
+          </p>
+        </article>
+      </article>
+    </section>
+  );
 }
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,16 +1,35 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { getCryptoCoinsAsync } from '../redux/crypto/cryptoCoinSlice';
 
 export default function Home() {
   const dispatch = useDispatch();
-  const cryptoCoins = useSelector((state) => state.cryptoCoins);
-  console.log('Home:', cryptoCoins);
   useEffect(() => {
     dispatch(getCryptoCoinsAsync());
   }, [dispatch]);
 
+  const { cryptoCoins } = useSelector((state) => state.cryptoCoins);
+
+  if (!cryptoCoins) {
+    return <p>Noo coins loaded</p>;
+  }
+
   return (
-    <p>Home</p>
+    <section>
+      {
+        cryptoCoins.map((cryptoCoin) => (
+          <article key={cryptoCoin.id}>
+            <Link to={`touch-coin/details/${cryptoCoin.id}`}>Details</Link>
+            <img
+              src={cryptoCoin.icon}
+              alt={`${cryptoCoin.name}logo`}
+            />
+            <h2>{cryptoCoin.name}</h2>
+            <span>{cryptoCoin.totalSupply}</span>
+          </article>
+        ))
+      }
+    </section>
   );
 }
